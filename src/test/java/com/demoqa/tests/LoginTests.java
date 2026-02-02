910c96cc8a82ebf7530097292d8e54b4678d97f4
kozlianskahanna <demiyncevaanna@gmail.com>
2026-02-02 19:22:18 +0100

    CW.02.02.2026

---
+++ src/test/java/com/demoqa/tests/LoginTests.java
@@ -0,0 +1,24 @@
+package com.demoqa.tests;
+
+import com.demoqa.core.TestBase;
+import com.demoqa.pages.HomePage;
+import com.demoqa.pages.SidePanel;
+import com.demoqa.pages.bookStore.LoginPage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class LoginTests extends TestBase {
+
+    @BeforeEach
+    public void preconditions(){
+        new HomePage(driver).getBookStore();
+        new SidePanel(driver).getLogin();
+    }
+
+    @Test
+    public void loginPositiveTest(){
+        new LoginPage(driver).enterUserData("HannaKozlianska","Anna14031981$")
+                .clickOnLoginButton()
+                .verifyUserName("HannaKozlianska");
+    }
+}
